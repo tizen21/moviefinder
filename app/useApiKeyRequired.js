4c17a6ed8560a475b34c00acb1ec47315969bf74
Antho <rey.anthony1@gmail.com>
2024-07-10 12:10:37 +0200

    1st commit

---
+++ app/useApiKeyRequired.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+
+export const useApiKeyRequired = () => {
+  useEffect(() => {
+    let cleanup = false;
+    const localStorageApiKey = localStorage.getItem("omdbApiKey");
+
+    if (!localStorageApiKey) {
+      while (!localStorage.getItem("omdbApiKey")) {
+        const apiKey = prompt("Quel est ton OMDB API KEY ?");
+        if (apiKey) {
+          localStorage.setItem("omdbApiKey", apiKey);
+        }
+        if (cleanup) break;
+      }
+    }
+    return () => {
+      cleanup = true;
+    };
+  }, []);
+};
